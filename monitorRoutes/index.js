7989826a79cd46e48f7aca7df38d742dde022820
40233306 <godonovan01@qub.ac.uk>
2021-12-07 01:51:17 +0000

    performance/timing for wordCount URL1

--- monitorRoutes/index.js
+++ monitorRoutes/index.js
@@ -12,6 +12,7 @@ router.get('/', async (req, res) => {
 
     
     try {
+        t = performance.now()
         const params = new URLSearchParams({
             [API_KEY_NAME]: API_KEY_VALUE,
             ...url.parse(req.url, true).query
@@ -20,7 +21,8 @@ router.get('/', async (req, res) => {
         const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
         data = apiRes.body
         
-
+        t2 = performance.now()
+        t3 = t2 - t
 
         //logging request to the console
         if(process.env.NODE_ENV !== 'production'){
@@ -34,21 +36,21 @@ router.get('/', async (req, res) => {
             {
                 if(data.error == false)
                 {
-                    data = "URL 1 for WordCount working as expected"
+                    data = "URL 1 for WordCount working as expected. Time taken: " + t3.toFixed(2) + " milliseconds."
                 }
                 else{
                     dataError = data.error
-                    data = "error expected was: false received: " + dataError
+                    data = "error expected was: false received: " + dataError + ". Time taken: " + t3.toFixed(2) + " milliseconds."
                 }
             }
             else{
                 dataString = data.string
-                data = "String expected was: Contains 12 words received: " + dataString
+                data = "String expected was: Contains 12 words received: " + dataString + ". Time taken: " + t3.toFixed(2) + " milliseconds."
             }
         }
         else{
             dataAnswer = data.answer
-            data = "Answer expected was 12 received: " + dataAnswer
+            data = "Answer expected was 12 received: " + dataAnswer + ". Time taken: " + t3.toFixed(2) + " milliseconds."
         }
         //console.log(data)
         res.status(200).json(data)
