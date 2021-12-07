5107affb812309c0827b0119fd127cdfeee24445
40233306 <godonovan01@qub.ac.uk>
2021-12-07 01:39:07 +0000

    monitor URL 1 for wordcount

--- monitorRoutes/index2v2.js
+++ monitorRoutes/index2v2.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 const needle = require('needle')
 
-const API_BASE_URL = process.env.API_BASE_URL
+const API_TEST_URL = process.env.API_TEST_URL2
 const API_KEY_NAME = process.env.API_KEY_NAME
 const API_KEY_VALUE = process.env.API_KEY_VALUE
 
@@ -14,13 +14,14 @@ router.get('/', async (req, res) => {
             ...url.parse(req.url, true).query
         })
 
-        const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
+        const apiRes = await needle('get', `${API_TEST_URL}?${params}`)
         const data = apiRes.body
 
         //logging request to the console
         if(process.env.NODE_ENV !== 'production'){
-            console.log(`REQUEST: ${API_BASE_URL}?${params}`)
+            console.log(`REQUEST: ${API_TEST_URL}?${params}`)
         }
+
         res.status(200).json(data)
     } catch (error) {
         res.status(500).json({ error })
--- monitorRoutes/indexv2.js
+++ monitorRoutes/indexv2.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 const needle = require('needle')
 
-const API_BASE_URL = process.env.API_BASE_URL
+const API_BASE_URL = process.env.API_BASE_URL2
 const API_KEY_NAME = process.env.API_KEY_NAME
 const API_KEY_VALUE = process.env.API_KEY_VALUE
 
@@ -21,6 +21,7 @@ router.get('/', async (req, res) => {
         if(process.env.NODE_ENV !== 'production'){
             console.log(`REQUEST: ${API_BASE_URL}?${params}`)
         }
+
         res.status(200).json(data)
     } catch (error) {
         res.status(500).json({ error })
