5045769c7729116fb1a8fb000defca46e3e35b41
40233306 <godonovan01@qub.ac.uk>
2021-12-11 20:45:06 +0000

    storage of text in database

--- index.js
+++ index.js
@@ -4,11 +4,86 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
 
+const Datastore = require('nedb');
+const { response } = require('express');
+
+//const pathToData = path.resolve(__dirname, "db/db")
+//const db = new Datastore({ filename: pathToData});
+//db.loadDatabase();
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+//database.insert({name: 'glen', status: 'good'});
+//database.insert({name: 'nova', status: 'bad'});
+
 const app = express()
 
 app.use(express.static('src'))
 
+app.use(express.json());
+
 app.set('trust proxy', 1)
+
+/* app.get("/api", (req, res) => {    
+    db.find({}, function (err, docs) {
+        if(err){
+            return err;
+        } 
+        res.json(docs);
+    });
+});
+
+app.post("/api", (req, res) => {
+    // our unix timestamp
+    const unixTimeCreated = new Date().getTime();
+    // add our unix time as a "created" property and add it to our request.body
+    const newData = Object.assign({"created": unixTimeCreated}, req.body)
+
+    // add in our data object to our database using .insert()
+    db.insert(newData, (err, docs) =>{
+        if(err){
+            return err;
+        }
+        res.json(docs);
+    });
+}) */
+
+
+app.post('/tester', (request, response) => {
+    console.log('I got a request');
+    const data = request.body;
+    database.insert(data);
+    //console.log(database);
+    response.json(data);
+});
+
+app.get('/getTester', (request, response) => {
+    database.find({}, (err, data) => {
+        if(err) {
+            response.end();
+            return;
+        }
+        response.json(data);
+    })
+    
+})
+
+
+
+/* app.post('/tester', (request, response) => {
+    console.log('I have a request');
+    console.log(request.body);
+    const data = request.body;
+    database.push(data);
+    console.log(database);
+    response.json({
+        status: 'success',
+        text: data.string
+    });
+}); */
+
+
+
    
 
 app.use('/api', require('./routes/index'))
